b5024e1a5818bf79d643b353a5e17221be675ec6
Mendel Minkowicz <mminkowicz@gmail.com>
2025-07-23 14:40:50 -0400

    Completed phase-0-js-arrays-lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,36 @@
-// Write your code here
+//Products Array//
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+// Console Log Details//
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+//Add a product function//
+function addProduct(productName) {
+  products.push(productName);
+}
+
+addProduct("Keyboard");
+console.log(products);
+
+
+// Update product name function//
+function updateProductName(index, newName) {
+  products[index] = newName;
+}
+
+updateProductName(1, "Smartphone");
+console.log(products);
+
+// Remove the last product function//
+function removeLastProduct() {
+  products.pop();
+}
+
+removeLastProduct();
+
+console.log(products)
 
 
 // Export the necessary parts for testing
